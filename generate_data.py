c249c3696956bb2fa54895ce76929b11ebfa21f2
JianWu <xcb479@alumni.ku.dk>
2017-09-19 23:02:42 +0200

    make it one descript

--- generate_data.py
+++ generate_data.py
@@ -7,30 +7,34 @@ import pickle
 import re
 import sqlite3 as sqlite
 
-dirpath = path.dirname(path.abspath('__file__'))
-orig_list = listdir(
-    path.join(dirpath, 'Thistles/jer_2016jan13n15/barley_10m/orig/'))
+def main():
+    dirpath = path.dirname(path.abspath('__file__'))
+    orig_list = listdir(
+        path.join(dirpath, 'Thistles/jer_2016jan13n15/barley_10m/orig/'))
 
-# build one connection to db file
-con = sqlite.connect('barley_10m.db')
-c = con.cursor()
-print('creating table...')
-c.execute('create table img(im_id, rgb, hsv)')
-print('table has been created')
+    # build one connection to db file
+    con = sqlite.connect('barley_10m.db')
+    c = con.cursor()
+    print('creating table...')
+    c.execute('create table img(im_id, rgb, hsv)')
+    print('table has been created')
 
-for orig_name in orig_list:
-    img_id = re.split('\.|_', orig_name)[1]
-    img_rgb = plt.imread(
-        path.join('Thistles/jer_2016jan13n15/barley_10m/orig', orig_name))
-    img_hsv = cv2.cvtColor(img_rgb,cv2.COLOR_BGR2HSV)
-    
-    c.execute(
-        'insert into img(im_id, rgb, hsv) values(?, ?, ?)',
-        (img_id, pickle.dumps(img_rgb), pickle.dumps(img_hsv))
-    )
-    con.commit()
-    print('Inserting img: {}'.format(img_id))
-    
+    for orig_name in orig_list:
+        img_id = re.split('\.|_', orig_name)[1]
+        img_rgb = plt.imread(
+            path.join('Thistles/jer_2016jan13n15/barley_10m/orig', orig_name))
+        img_hsv = cv2.cvtColor(img_rgb,cv2.COLOR_BGR2HSV)
+        
+        c.execute(
+            'insert into img(im_id, rgb, hsv) values(?, ?, ?)',
+            (img_id, pickle.dumps(img_rgb), pickle.dumps(img_hsv))
+        )
+        con.commit()
+        print('Inserting img: {}'.format(img_id))
+        
 
-con.close()
+    con.close()
 
+
+if __name__ == '__main__':
+    main()
