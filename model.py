c65327e5b0fc0f2c28cc9d302967304d24ba9b7b
JianWu <xcb479@alumni.ku.dk>
2017-10-04 13:06:54 +0200

    improve the model

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
